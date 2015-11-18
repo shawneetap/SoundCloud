f1242e03e0d3770931d7ef34fba4bcc6adb9a588
shawneetap <seanrobinson00@gmail.com>
2015-11-17 21:41:32 -0800

    added watch to Gruntfile.js @rgupta-okta

--- assets/grunt/Gruntfile.js
+++ assets/grunt/Gruntfile.js
@@ -1,5 +1,16 @@
 module.exports = function(grunt) {
 	grunt.initConfig({
+		watch: {
+		  js: {
+		    files: ['../js/*.js'],
+		    tasks: ['concat'],
+		  },
+		  css: {
+		  	files: ['../css/**/*.sass'],
+		    tasks: ['sass'],
+		  },
+		},
+
 		sass: {
 			dist: {
 				files: {
@@ -16,6 +27,8 @@ module.exports = function(grunt) {
 		}
 	});
 
+	grunt.loadNpmTasks('grunt-contrib-watch');
 	grunt.loadNpmTasks('grunt-contrib-concat');
 	grunt.loadNpmTasks('grunt-contrib-sass');
+	grunt.registerTask('default', ['concat', 'sass', 'watch']);
 };
